6883eb9de50fb6a520dfd1c3e1e3f7c56587c8b2
giffyteddy <vasundhra99@gmail.com>
2018-12-01 20:42:38 -0600

    button opens a new activity.

--- app/src/main/java/com/example/mnhar/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/mnhar/cs125finalproject/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.mnhar.cs125finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button searchButton;
+    private TextView month;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        month = findViewById(R.id.monthEdit);
+        searchButton = (Button) findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = month.getText().toString();
+                openSearchActivity();
+            }
+        });
+    }
+
+    private void openSearchActivity() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mnhar/cs125finalproject/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.mnhar.cs125finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SearchActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
