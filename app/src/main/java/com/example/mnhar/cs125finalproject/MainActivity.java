3d986a09a198a15fef029ac86fcb17ec0635d1a5
mnh31 <42979832+mnh31@users.noreply.github.com>
2018-12-05 11:54:05 -0600

    Work on back end accessing info from API

--- app/src/main/java/com/example/mnhar/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/mnhar/cs125finalproject/MainActivity.java
@@ -6,29 +6,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import org.json.*;
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+
+
+//https://api.nytimes.com/svc/archive/v1/1900/12.json?api-key=%3Cdd1044029b4644999f1a7c225dafacca%3E
+//example URL
 
 public class MainActivity extends AppCompatActivity {
 
     private Button searchButton;
-    private EditText year;
-    private String yearS;
     private EditText month;
     private String monthS;
+    private EditText year;
+    private String yearS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        year = findViewById(R.id.yearEdit);
         month = findViewById(R.id.monthEdit);
+        year = findViewById(R.id.yearEdit);
 
         searchButton = (Button) findViewById(R.id.searchButton);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                yearS = year.getText().toString();
                 monthS = month.getText().toString();
+                yearS = year.getText().toString();
+
+                //NEED TO HANDLE WITH INCORRECT INPUT HERE
+
+                //Vasu, started work here on splitting up searches for multiple
+                //months and years. Will be split on commas for now.
+                String[] months = monthS.split(",");
+                String[] years = yearS.split(",'");
+                for (String year : years) {
+                    year = year.trim();
+                }
+                for (String month : months) {
+                    month = month.trim();
+                }
                 openSearchActivity();
             }
         });
